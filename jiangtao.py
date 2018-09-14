f752061a792cd8b62063feb42a459ae14ab67fcd
[jiangtao] <[124273317@qq.com]>
2018-09-14 16:19:22 +0800

    1111

--- jiangtao.py
+++ jiangtao.py
@@ -6,16 +6,31 @@
 # @File    : jiangtao.py
 # @Software: PyCharm
 # #
-import urllib3
-from urllib.request import urlopen
-import chardet
-html = urlopen("http://www.baidu.com")
 
-print(html.read())
-
-
+# import urllib3
+# from urllib.request import urlopen
+# import chardet
+#
+# if _name_ == "_main_":
+#
+# resp
+#
+# html = urlopen("http://www.baidu.com")
+#
+#
+#
+# print(html.read())
+#
+#
+#
+# charset = chardet.detect(html)
+#
+# print(charset)
 # import urllib3
 #
 # dir(urllib3)
 #
 # print(dir())
+
+
+
